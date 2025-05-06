b179c9f15938c542f8cd47eb4e5b3cbd0ff43af1
Loïc Bouvier <loic.bouvier80@gmail.com>
2025-05-06 16:22:54 +0200

    Exit builtin

--- src/main.cs
+++ src/main.cs
@@ -7,7 +7,7 @@ while (true)
     
     var command = Console.ReadLine();
 
-    Console.WriteLine($"{command}: command not found");  
+    Console.WriteLine($"{command}: command not found");
+    Console.WriteLine("$ exit 0");
+    break;
 }
-
-
