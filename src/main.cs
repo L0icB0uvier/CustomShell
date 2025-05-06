5987903b772434056bd2e0e81060103892402c92
Loïc Bouvier <loic.bouvier80@gmail.com>
2025-05-06 16:08:32 +0200

    Handle invalid commands

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-Console.Write("$ ");
-
 // Wait for user input
-Console.ReadLine();
+var command = Console.ReadLine();
+
+Console.Write($"{command}: command not found");
