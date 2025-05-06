968cd3a019aef9d59d4c46e460ad44b7e41c2e40
Loïc Bouvier <loic.bouvier80@gmail.com>
2025-05-06 16:30:34 +0200

    Add exit code

--- src/main.cs
+++ src/main.cs
@@ -7,9 +7,10 @@ while (true)
     
     var command = Console.ReadLine();
 
-    if (command == "exit")
+    if (command == "exit 0")
     {
         Environment.Exit(0);
     }
+    
     Console.WriteLine($"{command}: command not found");
 }
