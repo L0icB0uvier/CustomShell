52205a20049b0496acfea319ccd9b386b814b7f7
L0icB0uvier <loic.bouvier@prontonmail.com>
2025-05-07 16:47:49 +0200

    codecrafters submit [skip ci]

--- src/CommandHandlers/TypeCommandHandler.cs
+++ src/CommandHandlers/TypeCommandHandler.cs
@@ -5,8 +5,27 @@ public class TypeCommandHandler : ICommandHandler
     public string? HandleCommand(string[] commandArguments)
     {
         var commandArgument = commandArguments[0];
-        return CommandPrompt.Commands.ContainsKey(commandArgument) ? 
-            $"{commandArgument} is a shell builtin" : 
-            $"{commandArgument}: not found";
+        
+        if (CommandPrompt.Commands.ContainsKey(commandArgument))
+        {
+            return $"{commandArgument} is a shell builtin";
+        } 
+        
+        var environmentPath = Environment.GetEnvironmentVariable("PATH");
+
+        if (string.IsNullOrEmpty(environmentPath) == false)
+        {
+            var paths = environmentPath.Split(Path.PathSeparator);
+            foreach (var path in paths)
+            {
+                var commandPath = Path.Combine(path, commandArgument);
+                if (File.Exists(commandPath))
+                {
+                    return $"{commandArgument} is {commandPath}";
+                }
+            }
+        }
+        
+        return $"{commandArgument}: not found";
     }
 }
